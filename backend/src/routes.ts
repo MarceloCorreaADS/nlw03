712c1ff0d8b00b7a6929a97b289d78655ca81d5e
Marcelo Correa <marcelocorreaads@gmail.com>
2020-12-17 18:11:17 -0300

    Reorganizado as rotas de acesso restrito

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -9,17 +9,20 @@ import authMiddleware from "./middlewares/auth";
 const routes = Router();
 const upload = multer(uploadConfig);
 
+/* rotas que não precisam de token */
 routes.get('/orphanages', OrphanagesController.index);
 routes.get('/orphanages/:id', OrphanagesController.show);
 routes.post('/orphanages', upload.array('images'),OrphanagesController.create);
 
+/* Rota de autenticação de usuários */
+routes.post('/authenticate', UsersController.authenticate);
+
+/* Middleware que faz a verificação do token */
+routes.use(authMiddleware);
+/* rotas que só são acessadas com token válido */
+routes.get('/me/:id', UsersController.me);
 routes.get('/users', UsersController.index);
 routes.get('/users/:id', UsersController.show);
 routes.post('/users', UsersController.create);
-
-routes.post('/authenticate', UsersController.authenticate);
-
-routes.use(authMiddleware);
-routes.get('/me/:id', UsersController.me);
 
 export default routes;
